de04a6e97443c43a0d2c29d741b51c11a8185f7c
OlavOlsm <OlavOlsm@gmail.com>
2016-01-13 15:55:10 +0100

    Add mockito exercises

--- mockitoExercises/test/no/westerdals/pg4100/mockitolecture/assignment2/OrderRevisitetTest.java
+++ mockitoExercises/test/no/westerdals/pg4100/mockitolecture/assignment2/OrderRevisitetTest.java
@@ -1,17 +1,58 @@
 package no.westerdals.pg4100.mockitolecture.assignment2;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
-import no.westerdals.pg4100.mockitolecture.Warehouse;
-import no.westerdals.pg4100.mockitolecture.assignment1.Order;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.doReturn;
 
+import java.util.logging.Logger;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import no.westerdals.pg4100.mockitolecture.Warehouse;
+import no.westerdals.pg4100.mockitolecture.WarehouseImpl;
+
 public class OrderRevisitetTest {
+	
+	private OrderRevisitet fillableOrder;
+	private OrderRevisitet nonFillableOrder;
+	private String productName = "ProductName";
+	private int productQuantity = 10;
+	
+	public Warehouse getWarehouse() {
+		Warehouse warehouse = new WarehouseImpl();
+		addProductsToWarehouse(warehouse);		
+		return warehouse;
+	}
+	
+	public Warehouse getMockWarehouse() {
+		Warehouse warehouse = mock(Warehouse.class);
+		addProductsToWarehouse(warehouse);
+		return warehouse;
+	}
+	
+	public void addProductsToWarehouse(Warehouse warehouse) {
+		warehouse.add(productName, productQuantity);
+	}
+	
+	public Logger setMockLogger(OrderRevisitet order) {
+		Logger logger = mock(Logger.class);
+		order.setLogger(logger);
+		return logger;
+	}
+	
+	@Before
+	public void SetUp() {
+		//Arrange
+		fillableOrder = new OrderRevisitet(productName, 1);
+		nonFillableOrder = new OrderRevisitet(productName, 11);
+	}
 
 	@Test
 	/**
@@ -20,7 +61,14 @@ public class OrderRevisitetTest {
 	 * Assert that a mail is sent when the order is not filled.
 	 */
 	public void mailSentIfOrderIsNotFilled() {
-		fail("Not implemented");
+		// ARRANGE
+		Warehouse warehouse = getWarehouse();
+		MailServiceStub mailer = new MailServiceStub();
+		nonFillableOrder.setMailService(mailer);
+		// ACT
+		nonFillableOrder.fill(warehouse);
+		// ASSERT
+		assertEquals(1, mailer.messageCount());
 	}
 
 	@Test
@@ -30,7 +78,13 @@ public class OrderRevisitetTest {
 	 * Assert that a mail is NOT sent when the order is filled.
 	 */
 	public void mailNotSentWhenOrderIsFilled() {
-		fail("Not implemented");
+		// ARRANGE
+		Warehouse warehouse = getWarehouse();
+		MailServiceStub mailer = new MailServiceStub();
+		// ACT
+		fillableOrder.fill(warehouse);
+		// ASSERT
+		assertEquals(0, mailer.messageCount());
 	}
 
 	@Test
@@ -40,7 +94,16 @@ public class OrderRevisitetTest {
 	 * when filling order is written to log.
 	 */
 	public void runtimeExceptionOnFillIsWrittenToLog() {
-		fail("Not implemented");
+		// ARRANGE
+		Warehouse mockWarehouse = getMockWarehouse();
+		Logger logger = setMockLogger(fillableOrder);
+		when(mockWarehouse.hasInventory(anyString(), anyInt())).thenReturn(true);
+		doThrow(new RuntimeException()).when(mockWarehouse).remove(anyString(), anyInt());
+		// ACT
+		fillableOrder.fill(mockWarehouse);
+		// ASSERT/VERIFY
+		assertFalse(fillableOrder.isFilled());
+		verify(logger).info(anyString());
 	}
 
 	@Test
@@ -50,7 +113,17 @@ public class OrderRevisitetTest {
 	 * when filling order is written to log.
 	 */
 	public void runtimeExceptionOnFillIsWrittenToLogSpy() {
-		fail("Not implemented");
+		// ARRANGE
+		Warehouse warehouse = getWarehouse();
+		Warehouse warehouseSpy = spy(warehouse);
+		Logger logger = setMockLogger(fillableOrder);
+		// stubbing the remove-method in the spy
+		doThrow(new RuntimeException()).when(warehouseSpy).remove(anyString(), anyInt());
+		// ACT
+		fillableOrder.fill(warehouseSpy);
+		// ASSERT/VERIFY
+		assertFalse(fillableOrder.isFilled());
+		verify(logger).info(anyString());
 	}
 
 	@Test
@@ -62,14 +135,14 @@ public class OrderRevisitetTest {
 	 */
 	public void orderIsFilledWhenWarehouseCanProvide() {
 		// ARRANGE
-		Warehouse mockWarehouse = mock(Warehouse.class);
-		when(mockWarehouse.hasInventory("TALISKER", anyInt()))
+		Warehouse mockWarehouse = getMockWarehouse();
+		//	when(mockWarehouse.hasInventory("TALISKER", anyInt())).thenReturn(true);
+		when(mockWarehouse.hasInventory(anyString(), anyInt()))
 				.thenReturn(true);
-		Order order = new Order("TALISKER", 20);
 		// ACT
-		order.fill(mockWarehouse);
+		fillableOrder.fill(mockWarehouse);
 		// ASSERT/VERIFY
-		assertTrue(order.isFilled());
+		assertTrue(fillableOrder.isFilled());
 	}
 	
 	@Test
@@ -81,7 +154,8 @@ public class OrderRevisitetTest {
 	public void semanticsNotImportant() {
 		OrderRevisitet order = new OrderRevisitet("TALISKER", 20);
 		OrderRevisitet orderSpy = spy(order);
-		when(orderSpy.returnStringMethod()).thenReturn("something");
+		//when(orderSpy.returnStringMethod()).thenReturn("something");
+		doReturn("something").when(orderSpy).returnStringMethod();
 		assertTrue(orderSpy.returnStringMethod().equals("something"));
 	}
 }
